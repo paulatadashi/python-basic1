07391cbddfc7ac58ccdd730a4872146c6df3738a
paulatadashi <paulalopesssantoss@gmail.com>
2022-02-08 20:48:51 -0300

    Ficha de inscrição

---
+++ Aula2-Relatorio.py
@@ -0,0 +1,12 @@
+nome = input("Escreva seu nome: ")
+idade = input("Escreva sua idade: ")
+altura = input("Escreva sua altura: ")
+telefone = input("Escreva seu telefone: ")
+cpf = input("Escreva seu cpf: ")
+endereço = input("Escreva seu endereço: ")
+
+print("\nEsse é a sua Ficha de inscrição:")
+
+print("\nNome:",nome, "\nIdade:", idade, "\nAltura:", altura, "\nTelefone:" , telefone, "\nCpf:", cpf, "\nEndereço:", endereço)
+
+
