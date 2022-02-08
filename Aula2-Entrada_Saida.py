e8feea442c557da7ff8b67640da8fe68da9062a0
paulatadashi <paulalopesssantoss@gmail.com>
2022-02-08 20:46:49 -0300

    Nome, idade e altura

---
+++ Aula2-Entrada_Saida.py
@@ -0,0 +1,7 @@
+nome = input ("Escreva seu nome:")
+idade = input ("Escreva sua idade:")
+altura = input ("Escreva sua idade:")
+
+print(nome, "tem", idade, "anos e ", altura, "de altura")
+
+print(type (nome), type(idade), type(altura))
