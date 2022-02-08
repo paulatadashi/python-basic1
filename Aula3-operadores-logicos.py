0140adca06ea313a46c0c47c5f2c47b416c37ce4
paulatadashi <paulalopesssantoss@gmail.com>
2022-02-08 20:52:47 -0300

    If e Else: Comandos Básicos

---
+++ Aula3-operadores-logicos.py
@@ -0,0 +1,12 @@
+print("Menu: \n1 = Escreva Guilherme\n2 = Escreva João\n3 = Escreva Maria\n")
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+    print("Guilherme")
+elif opcao == "2":
+    print("João")
+elif opcao == "3":
+    print("Maria")
+else:
+    print("Opção Invalida")
