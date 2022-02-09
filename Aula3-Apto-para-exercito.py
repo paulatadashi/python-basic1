c5bf57851788a9c30be092d8685709bd5e63a427
paulatadashi <paulalopesssantoss@gmail.com>
2022-02-09 00:14:53 -0300

    Exercício com If, elif e else

---
+++ Aula3-Apto-para-exercito.py
@@ -0,0 +1,13 @@
+idade = input("Digite sua idade:")
+peso = input("Digite seu peso:")
+altura = input("Digite sua altura:")
+
+
+if idade == "18":
+        print("Você está apto para ingressar ao exército")
+elif peso == "60 Kilos":
+        print("Você está apto para ingressar ao exército")
+elif altura == "1.70":
+        print("Você está apto para ingressar ao exército")
+else:
+        print("Você não está apto ingressar ao exército")
