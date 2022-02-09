361c6ea28582b4f83d5f011a3df2f97860647ed7
paulatadashi <paulalopesssantoss@gmail.com>
2022-02-09 01:02:28 -0300

    Exercício corrigido

--- Aula3-Exercício-apto-para-exercito-corrigido.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Digite sua idade:"))
-peso = float(input("Digite seu peso:"))
-altura = float(input("Digite sua altura:"))
-
-if idade >= 18 and peso >=60 and altura >=1.70:
-    print("Você está apto a servir o exercito")
-
-else:
-     print("Você não está apto")
